27dfe8e219a643ffdc18892e95851c0fd5e427b6
Anderson Misson <missondeveloper@gmail.com>
2020-09-06 18:23:39 -0300

    Aula 02-09 - Operadores Aritimeticos

---
+++ src/br/com/kotrix/capitulo_02_09/OperadoresAritimeticos.java
@@ -0,0 +1,21 @@
+package br.com.kotrix.capitulo_02_09;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		
+		int soma = 2 + 10;
+		int subtracao = 6 - 10;
+		int multiplicacao = 8 * 3;
+		int divisao = 8 / 2;
+		int resto = 7 % 2;
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(multiplicacao);
+		System.out.println(divisao);
+		System.out.println(resto);
+
+	}
+
+}
