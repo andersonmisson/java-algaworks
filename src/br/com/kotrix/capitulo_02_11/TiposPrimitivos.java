20eb5948f7e5107a808ba0b5d934351cd3619434
Anderson Misson <missondeveloper@gmail.com>
2020-09-06 19:03:59 -0300

    Aula 02-11 - Tipos Primitivos

---
+++ src/br/com/kotrix/capitulo_02_11/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package br.com.kotrix.capitulo_02_11;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		double precoProduto = 9.43;
+		
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+		
+		char turmaAluno1 = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		long populacao1 = 650000;
+		System.out.println(populacao1);
+		
+		long populacaoMundial = 7000000000L;
+		System.out.println(populacaoMundial);
+		
+		float saldoConta = 1030.43F;
+		System.out.println(saldoConta);
+
+	}
+
+}
