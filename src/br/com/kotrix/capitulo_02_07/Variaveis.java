4a24ae4efe37a00b97ccd6754e3fb004d637bd54
Anderson Misson <missondeveloper@gmail.com>
2020-09-06 18:08:01 -0300

    Aula 02-07 - Variaveis

---
+++ src/br/com/kotrix/capitulo_02_07/Variaveis.java
@@ -0,0 +1,17 @@
+package br.com.kotrix.capitulo_02_07;
+
+public class Variaveis {
+	
+	public static void main(String[] args) {
+		
+		int quantidade; // Declarando variavel inteiro
+		quantidade = 10; // atribuindo o valor 10
+		
+		System.out.println(quantidade);
+		
+		quantidade = 15;
+		
+		System.out.println(quantidade);
+	}
+
+}
