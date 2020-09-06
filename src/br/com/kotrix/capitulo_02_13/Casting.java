cff1486d261ec5edc5194347d50870ef4b9a5777
Anderson Misson <missondeveloper@gmail.com>
2020-09-06 19:39:01 -0300

    Aula 02-13 - Casting

---
+++ src/br/com/kotrix/capitulo_02_13/Casting.java
@@ -0,0 +1,25 @@
+package br.com.kotrix.capitulo_02_13;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		long x = 9300000035L;
+		
+		// casting
+		int y = (int)x;
+		System.out.println(y);
+		
+		int z = 10234;
+		
+		long z1 = z;
+		System.out.println(z1);
+		
+		double a = 20.5;
+		
+		float b = (float)a;
+		System.out.println(a);
+
+	}
+
+}
