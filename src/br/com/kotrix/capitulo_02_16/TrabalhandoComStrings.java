ab58233422463923de7c76af4d2bd7dccc84caed
Anderson Misson <missondeveloper@gmail.com>
2020-09-06 20:06:01 -0300

    Aula 02-16 - Strings

---
+++ src/br/com/kotrix/capitulo_02_16/TrabalhandoComStrings.java
@@ -0,0 +1,13 @@
+package br.com.kotrix.capitulo_02_16;
+
+public class TrabalhandoComStrings {
+
+	public static void main(String[] args) {
+		String nome = "Maria";
+		int idade = 30;
+		
+		System.out.println(nome + " tem " + idade + " anos.");
+
+	}
+
+}
