ba47ba67ccbcdb1a03d8d96d6f9c70e108514055
Anderson Misson <missondeveloper@gmail.com>
2020-09-07 01:19:57 -0300

    Aula 03-04 - Classe Carro2

---
+++ src/br/com/kotrix/capitulo_03_04/Principal.java
@@ -0,0 +1,15 @@
+package br.com.kotrix.capitulo_03_04;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Carro meuCarro = new Carro();
+		
+		Carro seuCarro = new Carro();
+		
+		
+
+	}
+
+}
