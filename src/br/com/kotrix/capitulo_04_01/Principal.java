51a80e1d406f12d2135b1435452444fc9b66077f
Anderson Misson <missondeveloper@gmail.com>
2020-09-10 11:29:15 -0300

    Aula 04-01 - Wrapper do Java

---
+++ src/br/com/kotrix/capitulo_04_01/Principal.java
@@ -0,0 +1,12 @@
+package br.com.kotrix.capitulo_04_01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int x = 10;
+		Integer i = new Integer(10);
+		byte b = i.byteValue();
+	
+	}
+	
+}
---
+++ src/br/com/kotrix/capitulo_04_01/TestesClassesWrapper.java
@@ -0,0 +1,30 @@
+package br.com.kotrix.capitulo_04_01;
+
+public class TestesClassesWrapper {
+
+	public static void main(String[] args) {
+		// byte, short, int,     long, float, double e char
+		// Byte, Short, Integer, Long, Float, Double e Character
+		
+		int x = 5;
+		Integer i = new Integer(5); // Isso é um Wrapper
+		
+		byte y = i.byteValue();
+		// xxxValue();
+		
+		String valor = "15.5";
+		Float v = new Float(valor); // Convertendo uma String para Float
+		System.out.println(v + 3);
+		
+		int idade = Integer.parseInt("27"); // parseXXX - outra forma de converter
+		System.out.println("Daqui a 5 anos você terá: " + (idade + 5) + " anos.");
+		
+		try {
+			double custo = Double.parseDouble("Vinte e tres reais e cinquenta centavos"); // Converter texto para inteiro é errado.
+			System.out.println("Custo total: " + custo);
+		} catch (NumberFormatException e) {
+			System.err.println("Numero inválido para conversão.");
+		}
+	}
+	
+}
