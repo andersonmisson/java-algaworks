84e28216f862eed7705853e564e373eec5d41817
Anderson Misson <missondeveloper@gmail.com>
2020-09-09 22:26:06 -0300

    Aula 03-13 - Argumentos por valor ou Referência

---
+++ src/br/com/kotrix/capitulo_03_13/Preco.java
@@ -0,0 +1,10 @@
+package br.com.kotrix.capitulo_03_13;
+
+public class Preco {
+
+	double valorCustos;
+	double valorImpostos;
+	double valorLucro;
+	double precoVenda;
+	
+}
---
+++ src/br/com/kotrix/capitulo_03_13/Produto.java
@@ -0,0 +1,14 @@
+package br.com.kotrix.capitulo_03_13;
+
+public class Produto {
+
+	void definirPreco(Preco preco, double percentualImpostos,
+			double margemLucro) {
+		preco.valorImpostos = preco.valorCustos 
+				* (percentualImpostos / 100);
+		preco.valorLucro = preco.valorCustos * (margemLucro / 100);
+		preco.precoVenda = preco.valorCustos 
+				+ preco.valorImpostos + preco.valorLucro;
+	}
+	
+}
