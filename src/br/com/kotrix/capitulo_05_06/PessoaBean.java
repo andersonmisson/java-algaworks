7b882483185ccf555aa88a689141b554559c0b7f
Anderson Misson <missondeveloper@gmail.com>
2020-09-11 15:13:54 -0300

    Aula 05-06 - JavaBean, Getter e Setter

---
+++ src/br/com/kotrix/capitulo_05_06/PessoaBean.java
@@ -0,0 +1,30 @@
+package br.com.kotrix.capitulo_05_06;
+
+public class PessoaBean {
+
+	private String nome;
+	private int idade;
+	
+	public PessoaBean() {
+		
+	}
+	
+	// setter e getter
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+}
