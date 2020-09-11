916a4afba49d7f0c00fd8d703da21420fb3c3846
Anderson Misson <missondeveloper@gmail.com>
2020-09-11 15:34:44 -0300

    Aula 05-07 - Exercicio Com UML

---
+++ src/br/com/kotrix/capitulo_05_07/Fornecedor.java
@@ -0,0 +1,14 @@
+package br.com.kotrix.capitulo_05_07;
+
+public class Fornecedor {
+	
+	public String nome;
+
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
