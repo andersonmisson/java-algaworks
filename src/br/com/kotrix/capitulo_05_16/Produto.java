e50fd72bc595110f8f6a8726065ba1efb3fbee80
Anderson Misson <missondeveloper@gmail.com>
2020-09-15 15:59:05 -0300

    Aula 05-16 - Sobreposição - super

---
+++ src/br/com/kotrix/capitulo_05_16/Produto.java
@@ -0,0 +1,11 @@
+package br.com.kotrix.capitulo_05_16;
+
+public class Produto {
+	
+	protected String descricao;
+	
+	public void identificar() {
+		System.out.println("Minha descrição é: " + descricao + ".");
+	}
+
+}
