880f2d7045c52bcdc059b74b94b8e55223c2cd07
Anderson Misson <missondeveloper@gmail.com>
2020-09-16 15:36:59 -0300

    Aula 05-18 - Sobrecarga

---
+++ src/br/com/kotrix/capitulo_05_18/TesteCadastro.java
@@ -0,0 +1,16 @@
+package br.com.kotrix.capitulo_05_18;
+
+public class TesteCadastro {
+
+	public static void main(String[] args) {
+		CadastroPessoa cadastro = new CadastroPessoa();
+		
+		Pessoa pessoa = new Pessoa("José", 32);
+		cadastro.cadastrar(pessoa);
+		
+		// -------------------------------------
+		
+		cadastro.cadastrar("Maria", 25);
+	}
+	
+}
