fa49f547eb9b0dd5f4e093308242c54dbddde73e
Anderson Misson <missondeveloper@gmail.com>
2020-09-21 03:07:32 -0300

    Aula 05-20 - Polimorfismo

---
+++ src/br/com/kotrix/capitulo_05_20/Conta.java
@@ -0,0 +1,15 @@
+package br.com.kotrix.capitulo_05_20;
+
+public class Conta {
+	
+	protected double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+}
---
+++ src/br/com/kotrix/capitulo_05_20/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package br.com.kotrix.capitulo_05_20;
+
+public class ContaPoupanca extends Conta{
+	
+	private double rendimentos = 30;
+	
+	public double getRendimentos() {
+		return rendimentos;
+	}
+
+}
