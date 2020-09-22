41812d48895959c425ee4966266033196c33a1e1
Anderson Misson <missondeveloper@gmail.com>
2020-09-22 11:44:03 -0300

    Aula 06-04 - Tratamento de exception

---
+++ src/br/com/kotrix/capitulo_06_04/SaldoInsuficienteException.java
@@ -0,0 +1,9 @@
+package br.com.kotrix.capitulo_06_04;
+
+public class SaldoInsuficienteException extends Exception{
+	
+	public SaldoInsuficienteException(String mensagem) {
+		super(mensagem);
+	}
+
+}
