f68f871beff908769dd7dc7e3a586ba59bec15ad
Anderson Misson <missondeveloper@gmail.com>
2020-09-28 15:47:35 -0300

    Aula 06-18 - Ordenando Objetos

---
+++ src/br/com/kotrix/capitulo_06_18/IdadeComparador.java
@@ -0,0 +1,18 @@
+package br.com.kotrix.capitulo_06_18;
+
+import java.util.Comparator;
+
+public class IdadeComparador implements Comparator<Pessoa> {
+
+	@Override
+	public int compare(Pessoa p1, Pessoa p2) {
+		if (p1.getIdade() < p2.getIdade()) {
+			return -1;
+		} else if (p1.getIdade() > p2.getIdade()) {
+			return 1;
+		}
+		
+		return 0;
+	}
+
+}
