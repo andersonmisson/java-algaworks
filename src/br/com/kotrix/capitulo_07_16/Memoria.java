959534e84a81486c4c7671f48931d92013587918
Anderson Misson <missondeveloper@gmail.com>
2020-10-07 20:08:15 -0300

    Aula 07-16 - Paramêtros da JVM

---
+++ src/br/com/kotrix/capitulo_07_16/Memoria.java
@@ -0,0 +1,33 @@
+package br.com.kotrix.capitulo_07_16;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Memoria {
+	
+	List<Object> lista = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Memoria memoria = new Memoria();
+		
+		try(Scanner entrada = new Scanner(System.in)){
+			int opcao = 0;
+			do {
+				System.out.println("Informe uma opção: ");
+				opcao = entrada.nextInt();
+				
+				if(opcao == 1) {
+					for (int i = 0; i < 2_000_000; i++) {
+						memoria.lista.add(new Object());
+					}
+				}
+				
+			} while(opcao !=0);
+			
+			
+		}
+
+	}
+
+}
