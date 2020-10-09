81031d6950040c7df552aaa3550baacfce0f4562
Anderson Misson <missondeveloper@gmail.com>
2020-10-09 14:46:18 -0300

    Aula 08-01 - Entrada E Saida I/O

---
+++ src/br/com/kotrix/capitulo_08_01/LendoArquivoItensPedido.java
@@ -0,0 +1,37 @@
+package br.com.kotrix.capitulo_08_01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class LendoArquivoItensPedido {
+
+	public static void main(String[] args) {
+
+		try (Scanner scanner = new Scanner(new BufferedReader(new FileReader("itens_pedido.txt")))) {
+			// O que delimita por padrão é o espaço, aqui eu avisei que era o ";"
+			scanner.useDelimiter(";");
+			// Para usar a virgula nos numero Brasileiros
+			Locale localeBrasil = new Locale("pt", "BR");
+			scanner.useLocale(localeBrasil);
+			// Aqui é para colocar R$ antes dos valores
+			NumberFormat formatador = NumberFormat.getCurrencyInstance(localeBrasil);
+			
+			while (scanner.hasNext()) {
+				String produto = scanner.next();
+				int quantidade = scanner.nextInt();
+				double valor = scanner.nextDouble();
+				scanner.nextLine(); // Se não colocar esse NextLine dentro do laço, ficara bagunçado
+				
+				System.out.printf("Produto: %s. Quantidade: %d. Por: %s\n",
+						produto, quantidade, formatador.format(valor));
+			}
+		} catch (IOException e) {
+			System.err.println("Erro abrindo arquivo. Erro: " + e.getMessage());
+		}
+	}
+	
+}
